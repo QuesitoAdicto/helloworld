2c53f58b1f34cd04eed0371b3f9d616566901205
Raul <quesitoadicto@gmail.com>
2019-10-03 17:34:27 -0500

    Body parser

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
 const express= require('express');
+const bodyparser= require('body-parser');
 const app = express();
 const pokedex= require('./pokedex.json');
 var datos = [];
 var Persona = {};
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
 app.get("/", (req, res) =>{
     console.log(req);
-    res.send("Hola mundo!");
+    res.send("Bienvenido al pokedex");
 });
-  app.get("/pokedex/id/:id", (req, res) =>{
+app.get("/pokedex", (req, res) =>{
+    res.send(pokedex.pokemon);
+});
+app.get("/pokemon", (req, res) =>{
+    res.json(req.body.x);
+});
+
+app.post("/pokemon", (req, res) =>{
+    res.json({message:"Post a /pokemon"});
+});
+  app.get("/pokedex/id/:id([0-9]{1,3})", (req, res) =>{
+    res.send(200);
     var dato = req.params.id;
 	  res.send(pokedex.pokemon[dato-1]);
   });
-  app.get("/pokedex/name/:name", (req, res) =>{
-    const filterCombiner = (d, filterArray) => {
-  for (let fn of filterArray) {
-    if (!fn(d)) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const filterArray1 = [
-  d => d.name === req.params.id,};
+  app.get("/pokedex/name/:name([A-Za-z]+)", (req, res) =>{
   });
-  app.get("/pokedex/random", (req, res) =>{
+  app.get("/pokedex/\\brandom\\b", (req, res) =>{
     var dato = Math.floor(Math.random() * 151);
 	  res.send(pokedex.pokemon[dato]);
     });
-  app.get("/pokedex/image/:id", (req, res) =>{
+  app.get("/pokedex/image/:id([0-9]{1.3})", (req, res) =>{
     var dato = req.params.id;
     const img=pokedex.pokemon[dato-1].img;
      res.send("<img src='"+img+"'/>");
+    });
+    app.use((req, res) =>{
+        res.status(404);
+        res.json({"404": "No existe la pagina"})
     });
 app.listen(3000, () =>{
     console.log("Server is running...");
